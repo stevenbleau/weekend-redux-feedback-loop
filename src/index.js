b7bf6a9028c29be64ab377d92766cc35392fa0c8
Steven Bleau <bleausd@gmail.com>
2022-09-18 19:45:24 -0500

    reducer setup in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore, CombineReducers} from 'redux';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//REDUCERS
+const reducer = (state = 0, action) => {
+    console.log('reducer!');
+    return state;
+}
+
+
+//STORE
+const storeInstance = createStore(
+    //reducer,
+    //reducer2,
+    //reducer3,
+)
+
+
+
+ReactDOM.render(
+    //WRAP ENTIRE APP IN PROVIDER
+    <Provider>
+        <App />,
+    </Provider>,
+     document.getElementById('root')
+);
 registerServiceWorker();
